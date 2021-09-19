a2306c3542bae2b8670d08ec49790535e69f882a
bungafebriani <90909529+bungafebriani@users.noreply.github.com>
2021-09-19 18:28:10 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"nama saya bunga febriani
+        saya tinggal didaerah bogor
+        olahraga yang saya sukai adalah
+        bulutangkis";
+    }
+}
